5ae604d17f23f0fbfd688570a948590e04072ff7
hoshina85 <hoshina85@gmail.com>
2016-12-08 13:09:47 +0900

    test

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hoshina85/test-ci/lib"
+	"github.com/serenize/snaker"
+)
 
 func main() {
 	var s string
 
-	if s, _ := hoge(); s != "" {
+	if s, _ := hello(); s != "" {
 		return
 	}
 	fmt.Println(s)
 }
 
-func hoge() (string, error) {
-	return "hello", nil
+func hello() (string, error) {
+	return snaker.CamelToSnake(lib.GetStr()), nil
 }
